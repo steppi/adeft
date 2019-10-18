b22edbf028c6748b0ae5affb967772ed44124421
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-10-18 17:17:10 -0400

    Implement cv method for AdeftAnomalyDetector

--- adeft/modeling/investigate.py
+++ adeft/modeling/investigate.py
@@ -1,12 +1,15 @@
 import gzip
 import json
 import logging
-from multiprocessing import Pool
 
+import numpy as np
 from sklearn.svm import OneClassSVM
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import ParameterGrid
 from sklearn.feature_extraction import TfidfVectorizer
+from sklearn.model_selection import KFold, GridSearchCV
+from sklearn.metrics import f1_score, precision_score, recall_score, \
+    make_scorer
+
 
 from adeft.nlp import english_stopwords
 
@@ -39,3 +42,34 @@ class AdeftAnomalyDetector(object):
         pipeline.fit(texts)
         self.estimator = pipeline
         self.best_score = None
+
+    def cv(self, pos_texts, neg_texts, param_grid, n_jobs=1, cv=5):
+        pipeline = Pipeline([('tfidf',
+                              TfidfVectorizer(stop_words=self.stop)),
+                             ('oc_svm',
+                              OneClassSVM())])
+        splits = KFold(n_splits=cv, shuffle=True).split(pos_texts)
+        X = pos_texts + neg_texts
+        y = [1.0]*len(pos_texts) + [-1.0]*len(neg_texts)
+        splits = ((train, np.concatenate((test,
+                                         np.arange(len(pos_texts), len(X)))))
+                  for train, test in splits)
+        f1_scorer = make_scorer(f1_score, pos_label=1.0,
+                                average='binary')
+        precision_scorer = make_scorer(precision_score,
+                                       pos_label=1.0,
+                                       average='binary')
+        recall_scorer = make_scorer(recall_score,
+                                    pos_label=1.0,
+                                    average='binary')
+        scorer = {'f1': f1_scorer, 'pr': precision_scorer,
+                  'rc': recall_scorer}
+        grid_search = GridSearchCV(pipeline, param_grid, n_jobs, cv=splits,
+                                   refit='f1', iid=False, scoring=scorer,
+                                   return_train_score=True)
+        grid_search.fit(X, y)
+        logger.info('Best f1 score of %s found for' % grid_search.best_score_
+                    + ' parameter values:\n%s' % grid_search.best_params_)
+        self.estimator = grid_search.best_estimator_
+        self.best_score = grid_search.best_score_
+        self.grid_search = grid_search

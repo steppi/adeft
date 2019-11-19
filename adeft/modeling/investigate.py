c30c5ce9a45ec9f57e5f91e1a8273b316a1dbe26
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-11-18 22:54:03 -0500

    Add computation of confidence interval

--- adeft/modeling/investigate.py
+++ adeft/modeling/investigate.py
@@ -6,6 +6,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import KFold, GridSearchCV
 from sklearn.feature_extraction.text import TfidfVectorizer
+from statsmodels.stats.proportion import proportion_confint
 
 
 from adeft.nlp import english_stopwords
@@ -26,20 +27,24 @@ class AdeftAnomalyDetector(object):
                   tokenize(' '.join(synonyms + [gene]))]
         # Add gene symbol and its synonyms to list of stopwords
         self.stop = set(english_stopwords).union(tokens)
+        self.__param_mapping = {'nu': 'oc_svm__nu',
+                                'max_features': 'tfidf__max_features',
+                                'ngram_range': 'tfidf__ngram_range'}
+        self.__inverse_param_mapping = {value: key for
+                                        key, value in
+                                        self.__param_mapping.items()}
 
-    def train(self, texts, kernel='linear', degree=3, gamma='scale', coef0=0.0,
-              nu=0.5, ngram_range=(1, 1), max_features=100):
+    def train(self, texts, nu=0.5, ngram_range=(1, 1), max_features=100):
         # initialize pipeline
         pipeline = Pipeline([('tfidf',
                               TfidfVectorizer(ngram_range=ngram_range,
                                               max_features=max_features,
                                               stop_words=self.stop)),
                              ('oc_svm',
-                              OneClassSVM(kernel=kernel, degree=degree,
-                                          gamma=gamma, coef0=coef0, nu=nu))])
+                              OneClassSVM(kernel='linear', nu=nu))])
+
         pipeline.fit(texts)
         self.estimator = pipeline
-        self.best_score = None
 
     def cv(self, pos_texts, neg_texts, param_grid, n_jobs=1, cv=10):
         pipeline = Pipeline([('tfidf',
@@ -56,24 +61,55 @@ class AdeftAnomalyDetector(object):
                                                               neg_splits))
         sensitivity_scorer = make_scorer(sensitivity_score, pos_label=-1.0)
         specificity_scorer = make_scorer(specificity_score, pos_label=-1.0)
-        sat_spec_scorer = make_scorer(satisfice_specificity_score,
-                                      pos_label=-1.0, min_spec=0.5)
+        se_scorer = make_scorer(specificity_score, pos_label=-1.0)
         scorer = {'sens': sensitivity_scorer, 'spec': specificity_scorer,
-                  'sat': sat_spec_scorer}
+                  'se': se_scorer}
+
+        param_grid = {self.__param_mapping[key]: value
+                      for key, value in param_grid.items()}
+
         grid_search = GridSearchCV(pipeline, param_grid, scoring=scorer,
                                    n_jobs=n_jobs, cv=splits, refit=False,
                                    iid=False)
         grid_search.fit(X, y)
-        logger.info('Best balanced accuracy score of %s found for'
-                    ' parameter values:\n%s' % (grid_search.best_score_,
-                                                grid_search.best_params_))
-        self.estimator = grid_search.best_estimator_
-        self.best_score = grid_search.best_score_
+        cv_results = grid_search.cv_results_
+        sensitivity, specificity, params = self._get_info(cv_results)
+        best_score = sensitivity + specificity - 1
+        logger.info('Best score of %s found for'
+                    ' parameter values:\n%s' % (best_score,
+                                                params))
+
+        self.sensitivity = sensitivity
+        self.specificity = specificity
+        self.best_score = best_score
+        self.best_params = params
         self.grid_search = grid_search
+        self.train(pos_texts, **params)
 
     def predict(self, texts):
         preds = self.estimator.predict(texts)
         return np.where(preds == -1.0, 1.0, 0.0)
+
+    def confidence_interval(self, texts, alpha=0.05):
+        preds = self.predict(texts)
+        p_hat = sum(preds)/len(preds)
+        print(len(preds), int(sum(preds)))
+        a, b = proportion_confint(sum(preds), len(preds), alpha=alpha,
+                                  method='wilson')
+        print(a, b)
+        left = max(0, (a - 1 + self.specificity)/self.best_score)
+        right = min(1, (b - 1 + self.specificity)/self.best_score)
+        return (left, right)
+
+    def _get_info(self, cv_results):
+        best_index = max(range(len(cv_results['mean_test_se'])),
+                         key=lambda i: cv_results['mean_test_se'][i])
+        sens = cv_results['mean_test_sens'][best_index]
+        spec = cv_results['mean_test_spec'][best_index]
+        params = cv_results['params'][best_index]
+        params = {self.__inverse_param_mapping[key]: value
+                  for key, value in params.items()}
+        return sens, spec, params
 
 
 def true_positives(y_true, y_pred, pos_label=1):
@@ -108,10 +144,7 @@ def specificity_score(y_true, y_pred, pos_label=1):
     return tn/(tn + fp)
 
 
-def satisfice_specificity_score(y_true, y_pred, pos_label=1,
-                                min_spec=0.5):
+def se_score(y_true, y_pred, pos_label=1):
     sens = sensitivity_score(y_true, y_pred, pos_label)
     spec = specificity_score(y_true, y_pred, pos_label)
-    if spec >= min_spec:
-        return sens
-    return 0.0
+    return sens + spec - 1
